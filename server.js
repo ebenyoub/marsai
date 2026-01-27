0384fdad45ddf0fba649f8c9b539832debfe847e
Daria Plishanova <daria.plishanova@laplateforme.io>
2026-01-27 16:34:17 +0100

    connection to the database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const db = require("./config/database");
 
 app.get('/', (req, res) => {
   res.send('Bienvenue sur mon application Express.js !');
