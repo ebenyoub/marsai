4ecee3af62717ab0d54afee9ed890b1c2016012d
nesrine-benlarbi <nesrine.benlarbi@laplateforme.io>
2026-01-28 16:27:52 +0100

    error, code dans dev

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,19 @@
-const mysql = require("mysql2");
+import mysql from 'mysql2'; // Import du type d'erreur
+import dotenv from 'dotenv';
+import type { QueryError } from 'mysql2';
 
-require("dotenv").config();
+dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306, // TS demande un nombre ici
 });
 
-connection.connect((error) => {
+// Typage de l'argument 'error'
+connection.connect((error: QueryError | null) => {
   if (error) {
     console.error("❌ Erreur de connexion à MySQL:", error.message);
     return;
@@ -17,4 +21,4 @@ connection.connect((error) => {
   console.log("✅ Connecté à la base de données MySQL");
 });
 
-module.exports = connection;
+export default connection;
