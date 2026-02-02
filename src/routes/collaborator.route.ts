50370c42927415f96aa2e2882e3cddb4a79fd4b4
Elyas B. <elyas.benyoub@laplateforme.io>
2026-02-02 13:46:59 +0100

    Remove file extension from collaborator controller import

--- src/routes/collaborator.route.ts
+++ src/routes/collaborator.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import collaboratorController from '../controllers/collaborator.controller.js';
+import collaboratorController from '../controllers/collaborator.controller';
 
 const router = express.Router();
 
