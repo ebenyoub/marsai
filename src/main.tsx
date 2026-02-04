b5a09bdb39bafc215cd2776e1683847d870d7ea2
Messioughi <ressane@MacBook-Air-de-Messioughi.local>
2026-02-04 10:18:17 +0100

    Frontend-Navbar+Language+Responsive+SEO

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App';
-import './index.css';
+import './styles/index.css';
+import { Navbar } from './components/Navbar.js';
+import { LanguageProvider } from './contexts/LanguageContext';
+import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <LanguageProvider>
     <BrowserRouter>
+    <div className="min-h-screen bg-background text-foreground">
+    <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<App />} />
         </Routes>
       </main>
+      </div>
     </BrowserRouter>
+    </LanguageProvider>
   </StrictMode>
 );
