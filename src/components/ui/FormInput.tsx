bee448a9d1cefb8a0b9dafa7b8f4910e88e31bb1
Daria Plishanova <daria.plishanova@laplateforme.io>
2026-02-10 09:30:09 +0100

    form prep

--- src/components/ui/FormInput.tsx
+++ src/components/ui/FormInput.tsx
@@ -8,8 +8,8 @@ export default function FormInput({
   ...props
 }: FormInputProps) {
     return(
-        <div>
-            <label htmlFor="">{label}</label>
+        <div className='flex flex-col  gap-2 w-full'>
+            <label className='text-sm text-white font-medium flex items-center'>{label}</label>
         </div>
     )
 }
