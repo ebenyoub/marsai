8817356a79b2b8b40b1c5f80ea333c754ceda208
Daria Plishanova <daria.plishanova@laplateforme.io>
2026-02-16 10:03:02 +0100

    film popup and example of the pagination, not finished

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import FilmsPageTest from '@/components/FilmPageTest';
-import Popup from '@/components/FilmPopup';
 import HeroSection from './components/HeroSection';
 import ProgramSection from './components/ProgramSection';
 import StatsSection from './components/StatsSection';
